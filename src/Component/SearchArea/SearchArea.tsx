7fe5d2f968374c1d0b52e0fea2946ca6d3b34ad8
mgahn0706 <mgahn0706@snu.ac.kr>
2022-06-27 00:25:00 +0900

    feature: 검색창 필터 url query 적용

--- src/Component/SearchArea/SearchArea.tsx
+++ src/Component/SearchArea/SearchArea.tsx
@@ -1,5 +1,35 @@
+import { useLocation, useNavigate } from "react-router";
+import qs from "qs";
 const SearchArea = () => {
-  return <input placeholder="배우고 싶은 언어, 기술을 검색해보세요" />;
+  const navigate = useNavigate();
+  const location = useLocation();
+  const handleSearch = (input: string) => {
+    const query = qs.parse(location.search, {
+      ignoreQueryPrefix: true,
+    });
+
+    const newQuery = {
+      ...query,
+      keyword: input,
+    };
+
+    const formattedQuery = new URLSearchParams(newQuery).toString();
+    console.log(newQuery);
+    console.log(formattedQuery);
+
+    input.length !== 0 || query.price
+      ? navigate(`/all?${formattedQuery}`)
+      : navigate(`/`);
+  };
+
+  return (
+    <input
+      placeholder="배우고 싶은 언어, 기술을 검색해보세요"
+      onChange={(e) => {
+        handleSearch(e.target.value);
+      }}
+    />
+  );
 };
 
 export default SearchArea;
